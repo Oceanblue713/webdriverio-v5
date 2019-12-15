8ae92e8fc3284fb3b0ab2d12fcbc3854b137e136
Anri Liy <anri@realquantum.com>
2019-12-15 11:09:13 -0600

    waitForDisplayed

--- test/wait-commands-test.js
+++ test/wait-commands-test.js
@@ -1,7 +1,7 @@
 describe("Test the various WebdriverIO Wait Commands",() => {
   beforeEach(function() {
     browser.url("./");
-  })
+  });
 
   it("Test waitForExist", () => {
     const clickByXpathSelector = $('//h1[text()="AJAX LOADER"]/..');
@@ -10,5 +10,15 @@ describe("Test the various WebdriverIO Wait Commands",() => {
 
     const clickMeButton = $('//*[text()="CLICK ME!"]/..');
     clickMeButton.waitForExist(10000);
+  });
+
+  it("Test waitForDisplayed", () => {
+    const clickByXpathSelector = $('//h1[text()="AJAX LOADER"]/..');
+    clickByXpathSelector.click();
+    browser.switchWindow("WebDriver | Ajax-Loader");
+
+    const clickMeButton = $('//*[text()="CLICK ME!"]/..');
+    clickMeButton.waitForDisplayed(10000);
+    clickMeButton.click();
   });
 });
