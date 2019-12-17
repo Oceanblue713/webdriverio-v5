a4ef1706b2e5e92751b5e9f8919bd3c610796247
Anri Liy <anri@realquantum.com>
2019-12-17 06:52:15 -0600

    Pause

--- test/wait-commands-test.js
+++ test/wait-commands-test.js
@@ -21,4 +21,28 @@ describe("Test the various WebdriverIO Wait Commands",() => {
     clickMeButton.waitForDisplayed(10000);
     clickMeButton.click();
   });
+
+  it("Test waitForEnabled", () => {
+    const clickByXpathSelector = $('//h1[text()="AJAX LOADER"]/..');
+    clickByXpathSelector.click();
+    browser.switchWindow("WebDriver | Ajax-Loader");
+
+    const clickMeButton = $('//*[text()="CLICK ME!"]/..');
+    // clickMeButton.waitForEnabled(10000, true);
+    clickMeButton.waitForEnabled(10000, false);
+
+    browser.pause(8000);
+    clickMeButton.waitForEnabled(10000, true);
+    clickMeButton.click();
+  });
+
+  it("Test Fixed Timeout",() => {
+    const clickByXpathSelector = $('//h1[text()="AJAX LOADER"]/..');
+    clickByXpathSelector.click();
+    browser.switchWindow("WebDriver | Ajax-Loader");
+
+    const clickMeButton =  $('//*[text()="CLICK ME!"]/..');
+    browser.pause(12000);
+    clickMeButton.click();
+  })
 });
